827a8bec49c6e4f7a36cdd728bd4107953e032d6
Ronald <r.vandevelde01@student.alfa-college.nl>
2023-11-14 16:20:05 +0100

    1.1.1.2
    beetje getest hoe de kotlin compose werkt

--- app/src/main/java/com/example/sheepplayer/MainActivity.kt
+++ app/src/main/java/com/example/sheepplayer/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.sheepplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sheepplayer.ui.theme.SheepPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +42,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+            Image(
+                painter = painterResource(R.drawable.cosmic_sheep),
+                contentDescription ="mijn schaap",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+            )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            // Add a vertical space between the author and message texts
+            //Spacer(modifier = Modifier.height(4.dp))
+            Text("leef!")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
